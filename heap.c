bafd2d8ab204396554c1960d430fe41fc1b22136
Martín Stefanelli <38985104+martinstd96@users.noreply.github.com>
2018-06-07 00:43:43 -0300

    Add new version of heap.c

--- heap.c
+++ heap.c
@@ -1,148 +1,127 @@
-/*
-  Hola marto, te comento que hay que organizar mas el codigo
-  en el header tendriamos q poner la deficion de los prototipos
-  auxiliares.
-
-  te comento q las funciones faltan optimizarlas. como el tema de redimensionar
-  el factor de carga, no lo tengo .. solo tengo lo q me diste de teoria
-
-  cabe destacar q no vamos a usar vector_dinamico.h pero vamos a tomar bastantes ideas de maximo_hijo
-  Bueno Exitos para fisica :P
-*/
-
-
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include "heap.h"
-#include <string.h>
 
-#define TAMANIO_INICIAL :P //modify for work
+#define TAMANIO_INICIAL 30
+#define TAMANIO_REDIMENSION 2
 
-/*struct*/
-struct heap {
+struct heap{
   size_t cantidad;
   size_t capacidad;
-  void** vector;
+  void** datos;
   cmp_func_t cmp;
 };
 
-/* prototipos funciones auxiliares */
-
-//retorna NULL en caso que este fuera de rango
-void* obtener(void** array,size_t n, size_t*pos);
-
-//retorna la posicion max
-//pre: valor1 y valor 2 son posiciones validas
-size_t obtener_max(void** array, size_t valor1, size_t valor2);
-
-
-
-
-heap_t *heap_crear(cmp_func_t cmp){
-  heap_t* heap = malloc( sizeof(heap_t) );
-
-  if(!heap) return NULL;
-
-  void** vector = vector_crear(TAMANIO_INICIAL);
-
-  if(!vector){
+heap_t* heap_crear(cmp_func_t cmp){
+  heap_t* heap=malloc(sizeof(heap_t));
+  if (!heap) return NULL;
+  void** datos=malloc(sizeof(void*)*TAMANIO_INICIAL);
+  if (!datos){
     free(heap);
     return NULL;
   }
-
-  heap->cantidad = 0;
-  heap->capacidad = TAMANIO_INICIAL;
-  heap->cmp = cmp;
-
+  heap->datos=datos;
+  heap->capacidad=TAMANIO_INICIAL;
+  heap->cantidad=0;
+  heap->cmp=cmp;
   return heap;
 }
 
-size_t heap_cantidad(const heap_t *heap){
+void swap(void* x,void* y){
+  void* auxiliar=*x;
+  *x=*y;
+  *y=auxiliar;
+}
+
+bool redimensionar_heap(heap_t* heap,size_t tamanio_nuevo){
+  void** datos_nuevos=realloc(heap->datos,sizeof(void*)*tamanio_nuevo);
+  if (!datos_nuevos) return false;
+  heap->datos=datos_nuevos;
+  heap->capacidad=tamanio_nuevo;
+  return true;
+}
+
+void upheap(void** arreglo,size_t posicion,cmp_func_t cmp){
+  if (posicion==0) return;
+  int padre=(posicion-1)/2;
+  if (cmp(arreglo[posicion],arreglo[padre])<0) return;
+  swap(&arreglo[posicion],&arreglo[padre]);
+  upheap(arreglo,padre,cmp);
+}
+
+bool heap_encolar(heap_t* heap,void* elem){
+  if (heap->cantidad==heap->capacidad){
+    size_t tamanio_nuevo=heap->capacidad*TAMANIO_REDIMENSION;
+    bool esta_redimensionado_heap=redimensionar_heap(heap,tamanio_nuevo);
+    if (!esta_redimensionado_heap) return false;
+  }
+  heap->datos[cantidad]=elem;
+  upheap(heap->datos,heap->cantidad,heap->cmp);
+  heap->cantidad++;
+  return true;
+}
+
+int comparar(void** arreglo,cmp_func_t cmp,size_t posicion1,size_t posicion2){
+  return cmp(arreglo[posicion1],arreglo[posicion2]);
+}
+
+void downheap(void** arreglo,size_t cantidad,size_t posicion_padre,cmp_func_t cmp){
+  if (posicion_padre>=cantidad) return;
+  size_t posicion_hijo_izquierdo=(2*posicion_padre)+1;
+  size_t posicion_hijo_derecho=(2*posicion_padre)+2;
+  size_t posicion_maximo=posicion_padre;
+  if (posicion_hijo_izquierdo<cantidad && comparar(arreglo,cmp,posicion_padre,posicion_hijo_izquierdo)<0 && comparar(arreglo,cmp,posicion_hijo_izquierdo,posicion_hijo_derecho)>0) posicion_maximo=posicion_hijo_izquierdo;
+  if (posicion_hijo_derecho<cantidad && comparar(arreglo,cmp,posicion_padre,posicion_hijo_derecho)<0 && comparar(arreglo,cmp,posicion_hijo_derecho,posicion_hijo_izquierdo)>0) posicion_maximo=posicion_hijo_derecho;
+  if (posicion_padre==posicion_maximo) return;
+  swap(&arreglo[posicion_padre],&arreglo[posicion_maximo]);
+  downheap(arreglo,cantidad,posicion_maximo,cmp);
+}
+
+void* heap_desencolar(heap_t* heap){
+  if (heap->cantidad==0) return NULL;
+  swap(&heap->datos[0],&heap->datos[heap->cantidad-1]);
+  void* dato_desencolado=heap->datos[heap->cantidad-1];
+  heap->cantidad--;
+  downheap(heap->datos,heap->cantidad,0,heap->cmp);
+  size_t un_cuarto_de_heap=heap->cantidad/4;
+  size_t tamanio_nuevo=heap->capacidad/TAMANIO_REDIMENSION;
+  if (un_cuarto_de_heap==heap->cantidad && tamanio_nuevo>=TAMANIO_INICIAL) redimensionar_heap(heap,tamanio_nuevo);
+  return dato_desencolado;
+}
+
+void* heap_ver_max(const heap_t* heap){
+  return heap->cantidad==0? NULL:heap->datos[0];
+}
+
+bool heap_esta_vacio(const heap_t* heap){
+  return heap->cantidad==0;
+}
+
+size_t heap_cantidad(const heap_t* heap){
   return heap->cantidad;
 }
 
-void *heap_ver_max(const heap_t *heap){
-  if ( heap->cantidad == 0 ) return NULL;
-
-  return vector_obtener(heap->vector, 0) // el maximo se encuentra en la posicion maxima del vector
-}
-
-bool heap_esta_vacio(const heap_t *heap){
-  return heap->cantidad == 0;
-}
-
-
-void uphead(void** arreglo, size_t pos, cmp_func_t cmp cmp){
-  if ( pos == 0 ) return;
-
-  //calculo padre
-  size_t padre = ( pos - 1 ) / 2;
-
-  if ( cmp ( arreglo[pos], arreglo[padre] ) < 0 ) return ;
-
-  swap( arreglo[pos], arreglo[padre] );
-  uphead ( arreglo , padre , cmp ) ;
-}
-
-
-
-bool heap_encolar(heap_t *heap, void *elem){
-	if (!elem) return false;
-
-  if (heap->cantidad == heap->capacidad ) redimensionar () // <--- redimensionar
-
-  // agrego el elemento al arreglo
-  // despues aplico upheap para que lo acomode solo
-  heap->array[ heap->cantidad ] = elem;
-  heap->cantidad++;
-
-  upheap(heap->arreglo, heap->cantidad, heap->cmp );
-	return true;
-}
-
-void downheap(void** array, size_t cantidad, size_t pibote ){
-  if (pibote >= cantidad) return ;
-
-  size_t pos_hijo_derecho = 2 * pibote + 1;
-  size_t pos_hijo_izquierdo = 2 * pibote + 2;
-
-  void* hijo_derecho = obtener(array, pos_hijo_derecho);
-  void* hijo_izquierdo = obtener(array, pos_hijo_izquierdo);
-
-  //termino de acomodar
-  if ( !hijo_izquierdo && !hijo_derecho ) return ;
-
-  //es por que existen los 2 hijos
-  if (hijo_derecho){
-    //calculo cual es el maximo
-    size_t maximo_hijo = obtener_max( hijo_izquierdo,hijo_derecho);
-
-    //cambio posiciones
-    swap(array[pibote],array[maximo_hijo]);
-
-    downheap(array,cantidad,maximo_hijo);
+void heap_destruir(heap_t* heap,void destruir_elemento(void* e)){
+  if (destruir_elemento){
+    for (size_t i=0; i<heap->cantidad; i++) destruir_elemento(heap->datos[i]);
   }
-
-  //si cai aca es por que solo existe el hijo derecho
-  swap(array[pibote],array[pos_hijo_izquierdo]);
-
-  downheap(array,cantidad,pos_hijo_izquierdo);
+  free(heap->datos);
+  free(heap);
 }
 
-// deficion de las funciones auxiliares
-
-void* obtener(void** array, size_t n, size_t pos){
-  return pos >= n ? NULL : array[pos];
+heap_t* heap_crear_arr(void* arreglo[],size_t n,cmp_func_t cmp){
+  heap_t* heap=malloc(sizeof(heap_t));
+  if (!heap) return NULL;
+  void** datos=malloc(sizeof(void*)*TAMANIO_INICIAL);
+  if (!datos){
+    free(heap);
+    return NULL;
+  }
+  heap->datos=datos;
+  heap->capacidad=TAMANIO_INICIAL;
+  heap->cantidad=0;
+  heap->cmp=cmp;
+  for (size_t i=0; i<n; i++) heap_encolar(heap,arreglo[i]);
+  return heap;
 }
-
-size_t obtener_max(void** array, size_t p, size_t p2){
-  return cmp (array[p] < array[p2]) ? p : p2;
-}
-
-/*
-        MACHETE :P
-        padre = (pos - 1) / 2
-        ultipadre = (len - 1 / 2);
-
-        hijo-izq = 2 * pos + 1
-        hijo-der = 2 * pos + 2
-
-*/
