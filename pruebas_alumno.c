16770c5eab81e7c4178d9ce747907d1c38df0353
Leonel R <leorhcp2030@gmail.com>
2018-06-08 00:06:31 -0300

    add test heap_vacio

---
+++ pruebas_alumno.c
@@ -0,0 +1,22 @@
+#include <string.h>
+
+void pruebas_heap_vacio(){
+	heap_t* heap = heap_crear(strcmp);
+
+	print_test("heap esta vacio", heap_cantidad(heap) == 0);
+	print_test("cantidad 0", heap_cantidad(heap) == 0);
+	print_test("desencolar es falso", !heap_desencolar(heap));
+	print_test("ver el maximo es falso", !heap_ver_max(heap));
+
+	heap_destruir(heap, NULL);
+	print_test("Destruyo el heap", true);
+}
+
+void pruebas_heap_alumno() {
+    //PRUEBAS ALUMNO
+	printf("~~Heap vacio~~:\n\n");
+	pruebas_heap_vacio();
+
+
+
+}
