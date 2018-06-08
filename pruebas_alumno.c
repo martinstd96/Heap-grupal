3745ca1f80304e58d87eba634b73ed8e154653ec
Leonel R <leorhcp2030@gmail.com>
2018-06-08 00:09:48 -0300

    modify test

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -3,7 +3,7 @@
 void pruebas_heap_vacio(){
 	heap_t* heap = heap_crear(strcmp);
 
-	print_test("heap esta vacio", heap_cantidad(heap) == 0);
+	print_test("heap esta vacio", heap_esta_vacio(heap) == 0);
 	print_test("cantidad 0", heap_cantidad(heap) == 0);
 	print_test("desencolar es falso", !heap_desencolar(heap));
 	print_test("ver el maximo es falso", !heap_ver_max(heap));
